d5904e4737997916c7f606b0d7726ba6571507a3
Abdullah Ridwan <abdullahridwan73@icloud.com>
2022-01-02 18:11:22 -0500

    Initial Commit

---
+++ CoreDataCRUD/CoreDataCRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataCRUDApp.swift
+//  CoreDataCRUD
+//
+//  Created by Abdullah Ridwan on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataCRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
